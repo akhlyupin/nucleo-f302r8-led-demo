6bc13d84ce8ef6256b80b00cdf7bd5ca4994ab38
Artem Khlyupin <trimp@yandex.ru>
2025-12-06 22:04:08 +0400

    cmake: enables c++20 standard

--- cmake/STM32F302.cmake
+++ cmake/STM32F302.cmake
@@ -37,6 +37,7 @@ endif()
 
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MMD -MP")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+set(CMAKE_CXX_STANDARD 20)
 
 set(CMAKE_C_LINK_FLAGS "${TARGET_FLAGS}")
 set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -T \"${CMAKE_SOURCE_DIR}/ld-script/STM32F302R8Tx_FLASH.ld\"")
